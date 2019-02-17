077f0959d2984575a267c56affd4a437af670720
ishikiyo615 <ishikiyo615@gmail.com>
2019-02-18 02:26:01 +0900

    StepAsideUnityChan Project First Commitment
    Lesson6課題

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour
+{
+    private int deadline = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //unitychanの位置情報を取り出す
+        Vector3 uni = GameObject.Find("unitychan").transform.position;
+        float z = uni.z;
+
+        if (transform.position.z < z + deadline)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
